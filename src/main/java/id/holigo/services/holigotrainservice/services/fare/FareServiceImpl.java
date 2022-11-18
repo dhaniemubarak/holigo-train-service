0d9c12e4d0fd3a3502f87737ec342503fdfa0898
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-11-18 10:23:29 +0700

    Fixed booked

--- src/main/java/id/holigo/services/holigotrainservice/services/fare/FareServiceImpl.java
+++ src/main/java/id/holigo/services/holigotrainservice/services/fare/FareServiceImpl.java
@@ -1,63 +1,24 @@
 package id.holigo.services.holigotrainservice.services.fare;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import id.holigo.services.common.model.FareDetailDto;
 import id.holigo.services.common.model.FareDto;
-import id.holigo.services.holigotrainservice.config.JmsConfig;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-
-@Slf4j
 @RequiredArgsConstructor
-//@Service
+@Service
 public class FareServiceImpl implements FareService {
 
-    private JmsTemplate jmsTemplate;
-
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    public void setJmsTemplate(JmsTemplate jmsTemplate) {
-        this.jmsTemplate = jmsTemplate;
-    }
-
-    @Autowired
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final FareServiceFeignClient fareServiceFeignClient;
 
     @Override
     public FareDto getFareDetail(FareDetailDto fareDetailDto) {
-        log.info("JMS getFareDetail is running...");
-        log.info("fareDetailDto -> {}", fareDetailDto);
-        Message message = jmsTemplate.sendAndReceive(JmsConfig.GET_DETAIL_FARE_PRODUCT, session -> {
-            Message detailFareMessage;
-            try {
-                detailFareMessage = session.createTextMessage(objectMapper.writeValueAsString(fareDetailDto));
-                detailFareMessage.setStringProperty("_type", "id.holigo.services.common.model.FareDetailDto");
-            } catch (JsonProcessingException e) {
-                throw new JMSException(e.getMessage());
-            }
-            return detailFareMessage;
-        });
-
-        assert message != null;
-        try {
-            log.info("fareDetailDto -> {}", message.getBody(String.class));
-        } catch (JMSException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            return objectMapper.readValue(message.getBody(String.class), FareDto.class);
-        } catch (JsonProcessingException | JMSException e) {
-            throw new RuntimeException(e);
-        }
+        ResponseEntity<FareDto> responseEntity = fareServiceFeignClient.getFare(
+                fareDetailDto.getUserId(),
+                fareDetailDto.getProductId(),
+                fareDetailDto.getNraAmount(),
+                fareDetailDto.getNtaAmount());
+        return responseEntity.getBody();
     }
 }
